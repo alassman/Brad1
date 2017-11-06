59c0296f6b2442486dd4d1c120ca6be931705265
lihuzhang <lihuzh@umich.edu>
2017-11-06 12:39:44 -0500

    new tellnext model trained with movie scriot

--- tellnext/__main__.py
+++
@@ -1,4 +0,0 @@
-import tellnext.tool
-
-if __name__ == '__main__':
-    tellnext.tool.new_next_word(word_1 = 'i')
--- tellnext_changed/tellnext/__main__.py
+++ tellnext_changed/tellnext/__main__.py
@@ -1,4 +1,4 @@
 import tellnext.tool
 
 if __name__ == '__main__':
-    tellnext.tool.new_next_word(word_1 = 'i')
+    tellnext.tool.main()
