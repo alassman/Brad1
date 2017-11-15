56a5d76ece5a0c73e1b286a316bc193037c5d47c
Rob Levy <roblevy@umich.edu>
2017-11-15 15:19:56 -0500

    made Button Push code a class

--- threadedDoublePress.py
+++ threadedDoublePress.py
@@ -10,9 +10,9 @@ class ButtonListener:
         # variables that Adam uses in his function
         self.Pressed = 0
         self.NotPressed = 1
-        self.Button1 = 23
-        self.Button2 = 24
-        self.Button3 = 25
+        self.left = 23
+        self.up = 24
+        self.right = 25
         # the interval over which to consider a double click
         self.MaxDelay = maxDelay #secs
         # disable double clicks when not in the main app
@@ -25,14 +25,14 @@ class ButtonListener:
 
     def setup_GPIO(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.Button1, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO23
-        GPIO.setup(self.Button2, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO24
-        GPIO.setup(self.Button3, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO25
+        GPIO.setup(self.left, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO23
+        GPIO.setup(self.up, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO24
+        GPIO.setup(self.right, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO25
 
     def launch(self):
         try:
             threads = []
-            buttonNums = [self.Button1, self.Button2, self.Button3]
+            buttonNums = [self.left, self.up, self.right]
             for i in range(3):
                 t = threading.Thread(target=self.buttonPress, args=(buttonNums[i], ))
                 threads.append(t)
@@ -44,17 +44,15 @@ class ButtonListener:
 
     def buttonPress(self, buttonNum):
     state = NotPressed
-    pulseCount = 0
     while True:
         # get input from the GPIO
         newState = GPIO.input(buttonNum)
         # main application or potentially tutorial???
         if self.allowDoubleClick:
             if newState == Pressed and state == NotPressed:
-                time.sleep(0.05)
                 # two button presses have occurred within the double-click interval
                 if(self.lastTime is not None):
-                    # potential double click
+                    # potential double click (same button)
                     if(self.lastPressed == buttonNum):
                         # valid double press
                         if time.time() < (self.lastTime + MaxDelay):
@@ -65,16 +63,18 @@ class ButtonListener:
                     else:
                         self.lastTime = time.time()
                         self.lastPressed = buttonNum
+                # Single push
                 else:
                     self.lastTime = time.time()
                     self.lastPressed = buttonNum
+                # Deals with debouncing
+                time.sleep(0.05)
             # no push --> not input to read
             elif newState == NotPressed:
                 state = NotPressed
-            
-            # check if the interval has expired to pick up a single press
+                        # check if the interval has expired to pick up a single press
             if (self.lastTime is not None 
-                and time.time() > (self.lastTime + self.MaxDelay)
+                and time.time() >= (self.lastTime + self.MaxDelay)
                 and self.lastPressed == buttonNum):
                     self.lastTime = None
                     print("Single Press", self.lastPressed)
@@ -83,8 +83,9 @@ class ButtonListener:
         else:
             if newState == Pressed and state == NotPressed:
                 print("Single Press", buttonNum)
+                state = Pressed
             elif newState == NotPressed:
-                state = NotPressedkb
+                state = NotPressed
 
     def reset(self):
         self.lastPressed = None
